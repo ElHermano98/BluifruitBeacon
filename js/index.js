89fd9132b47b984eb9d833aaef7611d774f5b47f
ElHermano98 <61493157+ElHermano98@users.noreply.github.com>
2020-03-05 11:56:33 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -74,7 +74,7 @@ function onError(reason)  {
 
 
 function test(){
-	var url='https://api.thingspeak.com/update?api_key=XFKFORMIUPKKREYE&field1=MARTIN';	
+	var url='https://api.thingspeak.com/update?api_key=QMNLNQ0ZTSKK3RUG&field1=MARTIN';	
 	openBrowser(url);
 }
 
